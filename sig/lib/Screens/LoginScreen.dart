2fe5f79cd217e82f6489d2f8487bdd38358cb641
Vipul Kumar Singh <Vipulrjput@gmail.com>
2022-07-17 15:12:25 +0530

    --- Auto Git Commit ---

--- sig/lib/Screens/LoginScreen.dart
+++ sig/lib/Screens/LoginScreen.dart
@@ -16,104 +16,125 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          // Image.asset('images/ggsipu.jpg'),
-          Expanded(
-            flex: 3,
-            child: Container(
-                height: 150.00,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Colors.blue[100],
-                  borderRadius: const BorderRadius.only(
-                    bottomLeft: Radius.circular(40),
-                    bottomRight: Radius.circular(40),
+      body: SafeArea(
+        child: Column(
+          children: [
+            // Image.asset('images/ggsipu.jpg',
+            // height : 200 ,
+            // width : 200,
+            // ),
+            Expanded(
+              flex: 3,
+              child: Container(
+                  height: 150.00,
+                  
+                  decoration: const BoxDecoration(
+                    color: Color.fromARGB(255, 221, 238, 250),
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(40),
+                      bottomRight: Radius.circular(40),
+                    ),
                   ),
+                  child: Column(
+                    children: const [
+                      Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          fontFamily: 'Pacifico',
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      TextField(
+                        // onChanged: (value) {
+                        //   email = value;
+                        // },
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                          labelText: 'E-mail address',
+                          hintText: 'Enter Your Email ID',
+                          labelStyle: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
+                            color: Colors.black,
+                          ),
+                          border: OutlineInputBorder(),
+                          filled: true,
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      TextField(
+                        // onChanged: (value) {
+                        //   email = value;
+                        // },
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                          labelText: 'Create Password',
+                          hintText: 'Enter your password',
+                          labelStyle: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
+                            color: Colors.black,
+                          ),
+                          border: OutlineInputBorder(),
+                          filled: true,
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      TextField(
+                        // onChanged: (value) {
+                        //   email = value;
+                        // },
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                          labelText: 'Confirm Password',
+                          hintText: 'Enter Your Email ID',
+                          labelStyle: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
+                            color: Colors.black,
+                          ),
+                          border: OutlineInputBorder(),
+                          filled: true,
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                    ],
+                  )),
+            ),
+
+            Expanded(
+              child: Container(
+                height: 20.00,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
                 ),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: const [
-                    Text('Sign Up'),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    TextField(
-                      // onChanged: (value) {
-                      //   email = value;
-                      // },
-                      textAlign: TextAlign.center,
-                      decoration:  InputDecoration(
-                        labelText: 'Email ID',
-                        hintText: 'Enter Your Email ID',
-                        labelStyle: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20.0,
-                          color: Colors.black,
-                        ),
-                        border: OutlineInputBorder(),
-                        filled: true,
+                    Text(
+                      'Login',
+                      style: TextStyle(
+                        fontFamily: 'Pacifico',
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
                       ),
-                      keyboardType: TextInputType.emailAddress,
                     ),
-
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    TextField(
-                      // onChanged: (value) {
-                      //   email = value;
-                      // },
-                      textAlign: TextAlign.center,
-                      decoration:  InputDecoration(
-                        labelText: 'Email ID',
-                        hintText: 'Enter Your Email ID',
-                        labelStyle: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20.0,
-                          color: Colors.black,
-                        ),
-                        border: OutlineInputBorder(),
-                        filled: true,
-                      ),
-                      keyboardType: TextInputType.emailAddress,
-                    ),
-
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    TextField(
-                      // onChanged: (value) {
-                      //   email = value;
-                      // },
-                      textAlign: TextAlign.center,
-                      decoration:  InputDecoration(
-                        labelText: 'Email ID',
-                        hintText: 'Enter Your Email ID',
-                        labelStyle: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20.0,
-                          color: Colors.black,
-                        ),
-                        border: OutlineInputBorder(),
-                        filled: true,
-                      ),
-                      keyboardType: TextInputType.emailAddress,
-                    ),
-                    
                   ],
-                )),
-          ),
-
-          Expanded(
-            child: Container(
-              height: 20.00,
-              decoration: const BoxDecoration(
-                color: Colors.white,
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
