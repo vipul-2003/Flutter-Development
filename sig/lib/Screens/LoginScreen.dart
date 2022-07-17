61d3f1e99692288c91e4af46c1dc5f02272d0a2c
Vipul Kumar Singh <Vipulrjput@gmail.com>
2022-07-17 17:17:09 +0530

    --- Auto Git Commit ---

--- sig/lib/Screens/LoginScreen.dart
+++ sig/lib/Screens/LoginScreen.dart
@@ -10,7 +10,7 @@ class loginScreen extends StatefulWidget {
 }
 
 class _loginScreenState extends State<loginScreen> {
-  final bool _secureText = true;
+  bool _secureText = true;
   late String email;
   late String password;
   @override
@@ -19,104 +19,134 @@ class _loginScreenState extends State<loginScreen> {
       body: SafeArea(
         child: Column(
           children: [
-            Image.asset('images/ggsipu.jpg',
-            height : 200 ,
-            width : 200,
-            ),
             Expanded(
               flex: 3,
               child: Container(
-                  height: 150.00,
-                  decoration: const BoxDecoration(
-                    color: Color.fromARGB(255, 221, 238, 250),
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(40),
-                      bottomRight: Radius.circular(40),
-                    ),
+                alignment :Alignment.center ,
+                height: 150.00,
+                decoration: const BoxDecoration(
+                  color: Color.fromARGB(255, 221, 238, 250),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(40),
+                    bottomRight: Radius.circular(40),
                   ),
-                  child: Column(
-                    children: const [
-                      Text(
-                        'Sign Up',
-                        style: TextStyle(
-                          fontFamily: 'Pacifico',
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
+                ),
+                child: Column(
+                  
+                  children: [
+                    const Text(
+                      'Sign Up',
+                      style: TextStyle(
+                        fontFamily: 'Pacifico',
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 50.0,
+                    ),
+                     TextFormField(
+                      // onChanged: (value) {
+                      //   email = value;
+                      // },
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        labelText: 'E-mail Address',
+                        hintText: 'Enter Your Email ID',
+                        // labelStyle: TextStyle(
+                        //   fontWeight: FontWeight.bold,
+                        //   fontSize: 20.0,
+                        //   color: Colors.black,
+                        // ),
+                          
+                          border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
                         ),
-                      ),
-                      SizedBox(
-                        height: 20.0,
-                      ),
-                      TextField(
-                        // onChanged: (value) {
-                        //   email = value;
-                        // },
-                        textAlign: TextAlign.center,
-                        decoration: InputDecoration(
-                          labelText: 'E-mail address',
-                          hintText: 'Enter Your Email ID',
-                          labelStyle: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20.0,
-                            color: Colors.black,
-                          ),
-                          border: OutlineInputBorder(),
-                          filled: true,
-                        ),
-                        keyboardType: TextInputType.emailAddress,
-                      ),
-                      SizedBox(
-                        height: 20.0,
-                      ),
-                      TextField(
-                        // onChanged: (value) {
-                        //   email = value;
-                        // },
-                        textAlign: TextAlign.center,
-                        decoration: InputDecoration(
-                          labelText: 'Create Password',
-                          hintText: 'Enter your password',
-                          labelStyle: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20.0,
-                            color: Colors.black,
-                          ),
-                          border: OutlineInputBorder(),
-                          filled: true,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 20.0,
-                      ),
-                      TextField(
-                        // onChanged: (value) {
-                        //   email = value;
-                        // },
-                        textAlign: TextAlign.center,
-                        decoration: InputDecoration(
-                          labelText: 'Confirm Password',
-                          hintText: 'Enter Your Email ID',
-                          labelStyle: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20.0,
-                            color: Colors.black,
-                          ),
-                          border: OutlineInputBorder(),
-                          filled: true,
-                        ),
-                      ),
-                    ],
-                  )),
-            ),
 
+                        filled: true,
+                      ),
+
+                      keyboardType: TextInputType.emailAddress,
+                      
+                    ),
+
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+
+                    TextField(
+                      // onChanged: (value) {
+                      //   email = value;
+                      // },
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        labelText: 'Create Password',
+                        hintText: 'Enter your password',
+                        labelStyle: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                          color: Colors.black,
+                        ),
+                        border: const OutlineInputBorder(),
+                        suffixIcon: IconButton(
+                          icon: Icon(_secureText
+                              ? Icons.remove_red_eye
+                              : Icons.security),
+                          onPressed: () {
+                            setState(() {
+                              _secureText = !_secureText;
+                            });
+                          },
+                        ),
+                      ),
+                      obscureText: _secureText,
+                      keyboardType: TextInputType.name,
+                    ),
+                    const SizedBox(
+                      height: 30.0,
+                    ),
+                    TextField(
+                      // onChanged: (value) {
+                      //   email = value;
+                      // },
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        labelText: 'Confirm Password',
+                        hintText: 'Qwerty@1234',
+                        labelStyle: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                          color: Colors.black,
+                        ),
+                        border: const OutlineInputBorder(),
+                        suffixIcon: IconButton(
+                          icon: Icon(_secureText
+                              ? Icons.remove_red_eye
+                              : Icons.security),
+                          onPressed: () {
+                            setState(() {
+                              _secureText = !_secureText;
+                            });
+                          },
+                        ),
+                      ),
+                      obscureText: _secureText,
+                      keyboardType: TextInputType.name,
+                    ),
+                  ],
+                ),
+              ),
+            ),
             Expanded(
               child: Container(
                 height: 20.00,
+                width: double.infinity,
                 decoration: const BoxDecoration(
                   color: Colors.white,
                 ),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: const [
                     Text(
                       'Login',
